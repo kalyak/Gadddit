04b54df649f51b63f3faf520188463770297661e
aukokyong <aukokyong@gmail.com>
2021-01-17 21:57:33 +0800

    updated main page with navbar

--- client/src/components/route/route-main.js
+++ client/src/components/route/route-main.js
@@ -4,12 +4,22 @@ import About from "../pages/main-about.js";
 import LoginPage from "../pages/main-Login";
 import SignupPage from "../pages/main-Signup";
 import AppURL from "./route-constants.js";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 const MainRoute = () => {
   return (
     <>
-      <h1>Home Route</h1>
-      <Router>
+      {/* <h1>Home Route</h1> */}
+
+      <Navbar bg="info" variant="dark" sticky="top">
+        <Navbar.Brand href="/user">Gaddit</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+      </Navbar>
+      {/* <Router>
         <div>
           <nav>
             <ul>
@@ -26,20 +36,20 @@ const MainRoute = () => {
               <li>
                 <Link to={AppURL.main.signup}>signup</Link>
               </li>
-            </ul>
+            </ul> */}
 
-            {/* START - Temporary - to be removed */}
-            <ul>
-              Temporary list:
-              <li>
-                <a href="/user/:userid">User</a>
-              </li>
-              <li>
-                <a href="/host/:userid">Host</a>
-              </li>
-            </ul>
-            {/* END - Temporary - to be removed */}
-          </nav>
+      {/* START - Temporary - to be removed */}
+      {/* <ul>
+        Temporary list:
+        <li>
+          <a href="/user/:userid">User</a>
+        </li>
+        <li>
+          <a href="/host/:userid">Host</a>
+        </li>
+      </ul> */}
+      {/* END - Temporary - to be removed */}
+      {/* </nav>
           <Switch>
             <Route exact path={AppURL.main.about}>
               <About />
@@ -58,7 +68,7 @@ const MainRoute = () => {
             </Route>
           </Switch>
         </div>
-      </Router>
+      </Router> */}
     </>
   );
 };
