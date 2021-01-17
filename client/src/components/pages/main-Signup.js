332cd1c5df011fe4e6f6a3e3351712d9511ade41
aukokyong <aukokyong@gmail.com>
2021-01-17 20:33:52 +0800

    updated signup page

--- client/src/components/pages/main-Signup.js
+++ client/src/components/pages/main-Signup.js
@@ -1,8 +1,69 @@
+import { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
 const LoginPage = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState("");
+  const [done, setDone] = useState(false);
+
+  const handleChange = (event) => {
+    setFormData((state) => {
+      return { ...state, [event.target.name]: event.target.value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+
+    axios
+      .post("/users", formData)
+      .then((response) => {
+        setDone(true);
+      })
+      .catch((error) => {
+        setErrors(error.message);
+      });
+  };
+
+  if (done) {
+    return <Redirect to />;
+  }
+
   return (
     <>
       <h1>Sign Up Page</h1>
       <p>To add in sign up form here</p>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <fieldset>
+          <legend>Create New Account</legend>
+          <label for="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            required
+            id="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label for="password">Password: </label>
+          <input
+            // type="password"
+            name="password"
+            required
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input type="submit" />
+          <p style={{ color: "red" }}>{errors}</p>
+        </fieldset>
+      </form>
     </>
   );
 };
