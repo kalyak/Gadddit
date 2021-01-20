0f4c8d66fbb67cad62fde0e99f07771cb8c8dc15
aukokyong <aukokyong@gmail.com>
2021-01-20 16:52:44 +0800

    removed temporary items from navbar

--- client/src/components/route/route-host.js
+++ client/src/components/route/route-host.js
@@ -7,24 +7,15 @@ const HostRoute = () => {
   return (
     <>
       {/* <h1>Host Route</h1> */}
-      <Navbar bg='danger' variant='light' sticky='top'>
-        <Navbar.Brand href='/host'>Gaddit</Navbar.Brand>
-        <Nav className='mr-auto'>
-          <Nav.Link href='/host'>Host Home</Nav.Link>
-          <Nav.Link href='/host/hosted'>Hosted</Nav.Link>
-          <Nav.Link href='/host/createroom'>Create Room</Nav.Link>
+      <Navbar bg="danger" variant="light" sticky="top">
+        <Navbar.Brand href="/host">Gaddit</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/host">Host Home</Nav.Link>
+          <Nav.Link href="/host/hosted">Hosted</Nav.Link>
+          <Nav.Link href="/host/createroom">Create Room</Nav.Link>
           <ChangeRoleBtn />
           <LogoutBtn />
         </Nav>
-
-        {/* START - Temporary - to be removed */}
-        <ul>
-          Temporary List:
-          <li>
-            <a href='/'>HOME</a>
-          </li>
-        </ul>
-        {/* END - Temporary - to be removed */}
       </Navbar>
     </>
   );
