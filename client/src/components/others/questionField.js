27c85f2712938762d155bfec8410a114e78685cf
aukokyong <aukokyong@gmail.com>
2021-01-20 11:58:18 +0800

    added question field to user view

---
+++ client/src/components/others/questionField.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+
+const QuestionField = (props) => {
+  const [question, setQuestion] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(question);
+
+    axios
+      .post(
+        `/qna/${props.roomId.roomid}/new`,
+        { question: question },
+        { withCredentials: true }
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleTextChange = (event) => {
+    console.log(event.target.value);
+    setQuestion(event.target.value);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          name="question"
+          placeholder="Input your question here"
+          onChange={handleTextChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default QuestionField;
